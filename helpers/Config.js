cce214aa482edee81b94d14528ac67e61f5f1636
ajay-turningpoint-vapi <ajay@turningpointvapi.com>
2024-12-21 09:48:33 +0530

    require('dotenv').config() added

--- helpers/Config.js
+++ helpers/Config.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
-dotenv.config({ path: "./.env" });
-
+// dotenv.config({ path: "./.env" });
+require('dotenv').config();
 export const CONFIG = {
     MONGOURI: process.env.MONGOURI,
     PORT: process.env.PORT,
